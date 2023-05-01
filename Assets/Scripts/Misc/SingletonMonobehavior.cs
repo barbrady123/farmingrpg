d4e3c1f808ece6503fe32ef70f771aa2c176fa06
David White <dwhite@us.oneilsoft.com>
2023-05-01 06:54:46 -0700

    thru 9

---
+++ Assets/Scripts/Misc/SingletonMonobehavior.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public abstract class SingletonMonobehavior<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get => _instance;
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
