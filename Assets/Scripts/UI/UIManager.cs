b47daf45a558914325f79737faeb322a527207ef
David White <dwhite@us.oneilsoft.com>
2023-05-17 05:40:17 -0700

    thru 72

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,94 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : SingletonMonobehavior<UIManager>
+{
+    private bool _pauseMenuOn = false;
+
+    [SerializeField]
+    private GameObject _pauseMenu = null;
+
+    [SerializeField]
+    private GameObject[] _menuTabs = null;
+
+    [SerializeField]
+    private Button[] _menuButtons = null;
+
+    public bool PauseMenuOn { get => _pauseMenuOn; set => _pauseMenuOn = value; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _pauseMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        PauseMenu();
+    }
+
+    private void PauseMenu()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_pauseMenuOn)
+            {
+                DisablePauseMenu();
+            }
+            else
+            {
+                EnablePauseMenu();
+            }
+        }
+    }
+
+    private void EnablePauseMenu()
+    {
+        _pauseMenuOn = true;
+        Player.Instance.PlayerInputIsDisabled = true;
+
+        Time.timeScale = 0;
+        _pauseMenu.SetActive(true);
+
+        // Trigger garbage collector
+        GC.Collect();
+
+        // Highlight selected button
+        HighlightButtonForSelectedTab();
+    }
+
+    private void DisablePauseMenu()
+    {
+        _pauseMenuOn = false;
+        Player.Instance.PlayerInputIsDisabled = false;
+        Time.timeScale = 1;
+        _pauseMenu.SetActive(false);
+    }
+
+    private void HighlightButtonForSelectedTab()
+    {
+        foreach (var tab in _menuTabs.WithIndex())
+        {
+            SetButtonColorActive(_menuButtons[tab.index], tab.item.activeSelf);
+        }
+    }
+
+    private void SetButtonColorActive(Button button, bool isActive)
+    {
+        var colors = button.colors;
+        colors.normalColor = isActive ? colors.pressedColor : colors.disabledColor;
+        button.colors = colors;
+    }
+
+    public void SwitchPauseMenuTab(int tabIndex)
+    {
+        foreach (var tab in _menuTabs.WithIndex())
+        {
+            tab.item.SetActive(tab.index == tabIndex);
+        }
+
+        HighlightButtonForSelectedTab();
+    }
+}
